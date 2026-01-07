43dd330a267bb958f91cee192200eab078d5ae4f
Couentinev2 <quent.guillaume@gmail.com>
2026-01-07 06:18:03 -0800

    Testing fix builder

--- app/[[...page]]/page.tsx
+++ app/[[...page]]/page.tsx
@@ -2,20 +2,21 @@ import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../src/components/builder"; 
 import "../../app/globals.css";
 
-
 export const revalidate = 5;
 
-// Initialize Builder
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 interface PageProps {
   params: Promise<{
     page: string[];
   }>;
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
 }
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
+  const searchParams = await props.searchParams; 
+  
   const urlPath = "/" + (params?.page?.join("/") || "");
 
   const content = await builder
@@ -23,6 +24,7 @@ export default async function Page(props: PageProps) {
       userAttributes: {
         urlPath: urlPath,
       },
+      options: searchParams, 
     })
     .toPromise();
 
